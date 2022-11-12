1bda1f02cce3c9efb965100f4397d893ee733094
Claire Fernandes <claire@hotdoc.com.au>
2022-11-12 13:02:36 +1100

    created restaurants controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+categories = %w[chinese italian japanese french belgian]
+
+5.times do
+  restaurant = Restaurant.create(
+    name: Faker::Movies::Lebowski.character,
+    address: Faker::Address.street_address,
+    category: categories.sample
+  )
+  puts "Created #{restaurant.name}"
+end
+
+puts 'Finished!'
